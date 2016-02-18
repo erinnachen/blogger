42d52a3a64338fb7396e1f9981500bff980a99f4
Erinna Chen <erinna.chen@gmail.com>
2016-02-18 14:25:11 -0700

    Iteration 1: Built out CRUD functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -7,4 +8,43 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      flash.notice = "Article '#{article.title}' Created!"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    if @article.save
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    Article.destroy(@article)
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to articles_path
+  end
+
+
 end
